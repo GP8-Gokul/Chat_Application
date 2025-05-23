0f4f6fe9a7235c98599608320779914e08366181
Jagannathan <jagannathan.n.1709@gmail.com>
2025-05-23 16:19:44 +0530

    Modified widgets

---
+++ flutterapp/lib/widgets/input_field.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatelessWidget {
+  final TextEditingController controller;
+  final String? errorText;
+  final String? hintText;
+
+  const InputField(
+      {super.key, required this.controller, this.errorText, this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        filled: true,
+        fillColor: Colors.white,
+        hintText: hintText,
+        prefixIcon: const Icon(
+          Icons.person,
+          color: Color(0xFF4A148C),
+        ),
+        errorText: errorText,
+      ),
+    );
+  }
+}
