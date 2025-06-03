970e4c68d2459668a7fbd65283505f864ecf8a5f
Gokul <gokulpjayan2004@gmail.com>
2025-06-03 13:47:54 +0530

    Remove MongoDB connection logging from mongodb.js

--- backend/data/mongodb.js
+++ backend/data/mongodb.js
@@ -4,8 +4,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/chatApp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
 
 const userSchema = new mongoose.Schema({
     username: String,
